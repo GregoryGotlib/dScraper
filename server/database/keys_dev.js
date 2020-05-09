0834668f071828fd282e33d94df9f489eec3d104
Gregory Gotlib <36772465+GregoryGotlib@users.noreply.github.com>
2020-05-09 19:15:49 +0300

    Delete keys_dev.js

--- server/database/keys_dev.js
+++
@@ -1,4 +0,0 @@
-module.exports ={
-    mongoURI: "mongodb+srv://gregorygotlib:gregory.gotlib@scrapercluster-zytxi.mongodb.net/test?retryWrites=true&w=majority",
-    secretOrKey:'you will never guess'
-};
